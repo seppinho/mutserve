a7b16eaea61ec81a9fa71aa5ca92ff4267737b2f
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-02-14 16:18:48 +0100

    adding test classes

---
+++ src/test/java/genepi/cnv/steps/HeteroplasmyModelTest.java
@@ -0,0 +1,37 @@
+package genepi.cnv.steps;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.apache.commons.math.MathException;
+import org.junit.Test;
+
+import genepi.cnv.util.RawFileAnalyser;
+
+public class HeteroplasmyModelTest {
+
+	@Test
+	public void HetModelTest() {
+
+		double hetLevel = 1;
+		String refPath = "test-data/mtdna/rcrs.fasta";
+		String sanger = "test-data/mtdna/sanger.txt";
+
+		RawFileAnalyser model = new RawFileAnalyser();
+		File input = new File("test-data/mtdna/raw-results/");
+
+		for (File file : input.listFiles()) {
+			System.out.println("input file is " + file.getName());
+			try {
+				boolean result = model.analyseFile(file.getPath(), refPath, sanger, hetLevel / 100);
+				assertEquals(true, result);
+			} catch (MathException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+}
