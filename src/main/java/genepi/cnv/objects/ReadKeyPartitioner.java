222d7d70c7582e71e57478e810716011d1973070
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-03-02 15:50:46 +0100

    adding new readkey partitioner

--- src/main/java/genepi/cnv/objects/ReadKeyPartitioner.java
+++ src/main/java/genepi/cnv/objects/ReadKeyPartitioner.java
@@ -1,12 +1,12 @@
 package genepi.cnv.objects;
 
-import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
+import org.seqdoop.hadoop_bam.SAMRecordWritable;
 
-public class ReadKeyPartitioner extends Partitioner<ReadKey, Text> {
+public class ReadKeyPartitioner extends Partitioner<ReadKey, SAMRecordWritable> {
 
 	@Override
-	public int getPartition(ReadKey key, Text value, int numPartitions) {
+	public int getPartition(ReadKey key, SAMRecordWritable value, int numPartitions) {
 		return Math.abs((int) ((key.getSample().hashCode())
 				* key.getSequence().hashCode() * 127))
 				% numPartitions;
