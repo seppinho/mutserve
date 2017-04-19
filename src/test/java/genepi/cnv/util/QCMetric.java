4a026f55c42111f6ec76e392d8bd654b8979b309
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-04-19 14:32:23 +0200

    add missing resource

---
+++ src/test/java/genepi/cnv/util/QCMetric.java
@@ -0,0 +1,34 @@
+package genepi.cnv.util;
+
+public class QCMetric {
+
+	private String id;
+	private double sensitivity;
+	private double precision;
+	private double specificity;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public double getSensitivity() {
+		return sensitivity;
+	}
+	public void setSensitivity(double sensitivity) {
+		this.sensitivity = sensitivity;
+	}
+	public double getPrecision() {
+		return precision;
+	}
+	public void setPrecision(double precision) {
+		this.precision = precision;
+	}
+	public double getSpecificity() {
+		return specificity;
+	}
+	public void setSpecificity(double specificity) {
+		this.specificity = specificity;
+	}
+}
