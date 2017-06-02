b05141b21b47549f39dbf20d88a68132475d7f91
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-06-02 11:36:50 +0200

    adding missing ressources

--- src/main/java/genepi/cnv/sort/SortTool.java
+++ src/main/java/genepi/cnv/sort/SortTool.java
@@ -1,10 +1,17 @@
 package genepi.cnv.sort;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+
+import com.google.common.cache.Cache;
+
+import genepi.cnv.objects.Extracter;
 import genepi.cnv.util.HadoopJobStep;
 import genepi.cnv.util.ReferenceUtil;
+import genepi.hadoop.CacheStore;
 import genepi.hadoop.HdfsUtil;
 import genepi.hadoop.common.WorkflowContext;
 import genepi.io.FileUtil;
@@ -18,26 +25,29 @@ public class SortTool extends HadoopJobStep {
 			String input = context.get("bwaOut");
 			String output = context.get("outputBam");
 			String inType = context.get("inType");
-			String reference = context.get("reference");
-			
-			String folder = getFolder(SortTool.class);
+			String archive = context.get("archive");
 
 			if (inType.equals("se") || inType.equals("pe")) {
 
 				List<String> folders;
 
 				folders = HdfsUtil.getDirectories(input);
-				
+
 				String[] inputs = new String[folders.size()];
 				for (int i = 0; i < folders.size(); i++) {
 					inputs[i] = folders.get(i);
 				}
+
+				String folder = archive.substring(0,archive.lastIndexOf("/")+1);
+				Extracter.extract(archive,folder);
+			
+				String ref = ReferenceUtil.findFileinDir(new File(folder),"fasta");
+				String reference = ReferenceUtil.readInReference(ref);
 				SortJob sort = new SortJob("sort-reads");
 				sort.setInput(inputs);
-				String ref = ReferenceUtil.readInReference(FileUtil.path(folder,reference + ".fasta"));
-				sort.setRefLength(ref.length() + "");
+				sort.setRefLength(reference.length()+"");
 				sort.setOutput(output);
-				
+
 				result = executeHadoopJob(sort, context);
 
 			}
