a90aaf260a7e3f821ce203b4f50670c781a57ebd
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2017-12-07 13:09:01 +0100

    update readtest

--- src/main/java/genepi/mut/pileup/ReadBamTest.java
+++ src/main/java/genepi/mut/pileup/ReadBamTest.java
@@ -3,6 +3,9 @@ package genepi.mut.pileup;
 import java.io.File;
 import java.io.IOException;
 
+import genepi.mut.objects.BasePosition;
+import htsjdk.samtools.CigarElement;
+import htsjdk.samtools.CigarOperator;
 import htsjdk.samtools.SAMRecord;
 import htsjdk.samtools.SAMRecordIterator;
 import htsjdk.samtools.SamReader;
@@ -12,13 +15,67 @@ public class ReadBamTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		final SamReader reader = SamReaderFactory.makeDefault().open(new File("test-data/mtdna/bam/test.bam"));
+		final SamReader reader = SamReaderFactory.makeDefault()
+				.open(new File("test-data/mtdna/bam/input/HG00096.mapped.ILLUMINA.bwa.GBR.low_coverage.20101123.bam"));
 
 		SAMRecordIterator d = reader.iterator();
 
 		while (d.hasNext()) {
-			SAMRecord record = d.next();
-			System.out.println(record.getSAMString());
+
+			SAMRecord samRecord = d.next();
+
+			if (samRecord.getReadName().equals("SRR062641.3033963")) {
+
+				System.out.println(samRecord.getSAMString());
+
+				Integer currentReferencePos = samRecord.getAlignmentStart();
+
+				int currentPos = 0;
+
+				for (CigarElement cigarElement : samRecord.getCigar().getCigarElements()) {
+
+					Integer cigarElementLength = cigarElement.getLength();
+
+					if (cigarElement.getOperator() == CigarOperator.D) {
+						
+						Integer cigarElementStart = currentReferencePos;
+
+						int i = 0;
+						while (i < cigarElementLength) {
+							System.out.println("D " + cigarElementStart);
+							i++;
+						}
+
+						//to get correct position for insertions
+						if (currentPos > 0) {
+							currentPos = currentPos - cigarElement.getLength();
+						}
+					}
+
+					if (cigarElement.getOperator() == CigarOperator.I) {
+
+						int i = 0;
+						while (i < cigarElementLength) {
+							char insBase = samRecord.getReadString().charAt(currentPos + i);
+							byte quality = samRecord.getBaseQualities()[currentPos + i];
+							i++;
+						}
+
+						currentPos = currentPos + cigarElement.getLength();
+
+					}
+
+					if (cigarElement.getOperator().consumesReferenceBases()
+							|| cigarElement.getOperator() == CigarOperator.SOFT_CLIP) {
+						currentReferencePos = currentReferencePos + cigarElement.getLength();
+						currentPos = currentPos + cigarElement.getLength();
+					}
+
+				}
+
+				System.exit(0);
+			}
+
 		}
 
 		try {
