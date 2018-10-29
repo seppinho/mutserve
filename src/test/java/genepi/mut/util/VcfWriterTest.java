6c1a29174e5cf1bd7a232a6d8056dd3f1afb6dd0
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2018-10-29 08:52:38 +0100

    Improve VcfWriter Test

--- src/test/java/genepi/mut/util/VcfWriterTest.java
+++ src/test/java/genepi/mut/util/VcfWriterTest.java
@@ -1,9 +1,10 @@
-package genepi.mut.steps;
+package genepi.mut.util;
 
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -11,6 +12,7 @@ import genepi.mut.objects.Sample;
 import genepi.mut.objects.Variant;
 import genepi.mut.util.MutationServerReader;
 import genepi.mut.util.VcfWriter;
+import htsjdk.variant.variantcontext.Allele;
 import htsjdk.variant.variantcontext.Genotype;
 import htsjdk.variant.variantcontext.VariantContext;
 import htsjdk.variant.vcf.VCFFileReader;
@@ -32,13 +34,15 @@ public class VcfWriterTest {
 		String sampleString = "test.cram";
 
 		Sample sample = samples.get(sampleString);
+		int countHomoplasmies = 0;
+		int countHeteroplasmies = 0;
 
 		for (Variant variant : sample.getVariants()) {
 			int pos = variant.getPos();
 			char var = variant.getVariant();
 
 			if (variant.getType() == 1) {
-
+				countHomoplasmies++;
 				for (final VariantContext vc : reader) {
 					if (vc.getStart() == pos) {
 						Genotype genotype = vc.getGenotype(sampleString);
@@ -48,7 +52,24 @@ public class VcfWriterTest {
 				}
 
 			}
+
+			if (variant.getType() == 2) {
+				for (final VariantContext vc : reader) {
+					if (vc.getStart() == pos) {
+						countHeteroplasmies++;
+						Genotype genotype = vc.getGenotype(sampleString);
+						genotype.getAlleles();
+						String txtGenotype = variant.getMajor() + "/" + variant.getMinor();
+						assertEquals(txtGenotype, genotype.getGenotypeString(true));
+					}
+
+				}
+
+			}
 		}
+
+		assertEquals(7, countHeteroplasmies);
+		assertEquals(38, countHomoplasmies);
 
 		reader.close();
 	}
