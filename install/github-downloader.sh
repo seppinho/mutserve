080a7f17350e6466f243177df03fe22b56c7acdb
seppinho <sebastian.schoenherr@i-med.ac.at>
2020-09-24 14:06:15 +0200

    Prepare release v2.0.0-rc6

--- install/github-downloader.sh
+++ install/github-downloader.sh
@@ -3,7 +3,7 @@
 set -e
 
 NAME="Mutserve"
-VERSION="v2.0.0-rc5"
+VERSION="v2.0.0-rc6"
 GITHUB_USER="seppinho"
 GITHUB_REPO="mutserve"
 EXECUTABLE="mutserve"
--- src/main/java/genepi/mut/App.java
+++ src/main/java/genepi/mut/App.java
@@ -23,7 +23,7 @@ public class App implements Runnable {
 
 	public static final String APP = "mtDNA Variant Detection";
 
-	public static final String VERSION = "v2.0.0-rc5";
+	public static final String VERSION = "v2.0.0-rc6";
 
 	public static final String COPYRIGHT = "(c) Sebastian Schoenherr, Hansi Weissensteiner, Lukas Forer";
 
