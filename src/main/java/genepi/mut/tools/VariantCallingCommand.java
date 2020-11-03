133fc224e6cba235fa2f06d5f25d668107cbf380
Sebastian Schoenherr <sebastian.schoenherr@i-med.ac.at>
2020-11-03 16:11:24 +0100

    Write correct contig info to VCF

--- src/main/java/genepi/mut/tools/VariantCallingCommand.java
+++ src/main/java/genepi/mut/tools/VariantCallingCommand.java
@@ -14,6 +14,9 @@ import genepi.mut.tasks.MergeTask;
 import genepi.mut.tasks.VariantCallingTask;
 import genepi.mut.util.FastaWriter;
 import genepi.mut.util.VcfWriter;
+import htsjdk.samtools.SAMSequenceDictionary;
+import htsjdk.samtools.reference.FastaSequenceFile;
+import htsjdk.samtools.reference.ReferenceSequence;
 import htsjdk.samtools.util.StopWatch;
 import lukfor.progress.TaskService;
 import lukfor.progress.tasks.Task;
@@ -100,15 +103,15 @@ public class VariantCallingCommand implements Callable<Integer> {
 		if (input.size() == 1 && new File(input.get(0)).isDirectory()) {
 			int count = 0;
 			for (File f : new File(input.get(0)).listFiles()) {
-				if(f.getName().endsWith("bam") || f.getName().endsWith("cram")) {
+				if (f.getName().endsWith("bam") || f.getName().endsWith("cram")) {
 					input.add(f.getAbsolutePath());
 					count++;
-				} 
+				}
 			}
 			System.out.println(count + " files added.");
 			input.remove(0);
 		}
-		
+
 		if (input == null || input.isEmpty()) {
 			System.out.println();
 			System.out.println("Please provide at least one indexed CRAM or BAM file.");
@@ -121,7 +124,7 @@ public class VariantCallingCommand implements Callable<Integer> {
 		if (!noFreq) {
 			InputStream in = this.getClass().getClassLoader().getResourceAsStream("1000g.frq");
 			freqFile = BayesFrequencies.instance(new DataInputStream(in));
-		} 
+		}
 
 		if (noAnsi) {
 			TaskService.setAnsiSupport(false);
@@ -145,36 +148,36 @@ public class VariantCallingCommand implements Callable<Integer> {
 
 		List<VariantCallingTask> tasks = new Vector<VariantCallingTask>();
 		int index = 0;
- 
+
 		for (String name : input) {
 
-				String varName = variantPath + ".tmp." + index;
+			String varName = variantPath + ".tmp." + index;
 
-				String rawName = null;
+			String rawName = null;
 
-				if (rawPath != null) {
-					rawName = rawPath + ".tmp." + index;
-				}
+			if (rawPath != null) {
+				rawName = rawPath + ".tmp." + index;
+			}
 
-				VariantCallingTask vc = new VariantCallingTask();
+			VariantCallingTask vc = new VariantCallingTask();
 
-				vc.setInput(name);
-				vc.setVarName(varName);
-				vc.setRawName(rawName);
-				vc.setFreqFile(freqFile);
-				vc.setLevel(level);
-				vc.setBaseQ(baseQ);
-				vc.setMapQ(mapQ);
-				vc.setAlignQ(alignQ);
-				vc.setBaq(baq);
-				vc.setDeletions(deletions);
-				vc.setInsertions(insertions);
-				vc.setReference(reference);
-				vc.setMode(mode);
-				vc.setContig(contig);
+			vc.setInput(name);
+			vc.setVarName(varName);
+			vc.setRawName(rawName);
+			vc.setFreqFile(freqFile);
+			vc.setLevel(level);
+			vc.setBaseQ(baseQ);
+			vc.setMapQ(mapQ);
+			vc.setAlignQ(alignQ);
+			vc.setBaq(baq);
+			vc.setDeletions(deletions);
+			vc.setInsertions(insertions);
+			vc.setReference(reference);
+			vc.setMode(mode);
+			vc.setContig(contig);
 
-				tasks.add(vc);
-				index++;
+			tasks.add(vc);
+			index++;
 
 		}
 
@@ -197,8 +200,19 @@ public class VariantCallingCommand implements Callable<Integer> {
 		TaskService.monitor(App.STYLE_SHORT_TASK).run(mergeTask);
 
 		if (output.endsWith("vcf.gz") || output.endsWith("vcf")) {
+
+			FastaSequenceFile fastaFile = new FastaSequenceFile(new File(reference), false);
+			final ReferenceSequence referenceSequence = fastaFile.nextSequence();
+
+			if (referenceSequence == null) {
+				System.out.println("Can not reference fasta file");
+				System.exit(-1);
+			}
+
 			VcfWriter vcfWriter = new VcfWriter();
-			vcfWriter.createVCF(variantPath, output, reference, "chrM", 16569, App.VERSION + ";" + App.COMMAND);
+			vcfWriter.createVCF(variantPath, output, reference, referenceSequence.getName(),
+					referenceSequence.getBaseString().length(), App.VERSION + ";" + App.COMMAND);
+			fastaFile.close();
 		}
 
 		if (writeFasta) {
